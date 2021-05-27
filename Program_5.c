fbeb71dab72fe63d19a9f06b2864c2f78f79d81a
nishanthss <>
2021-05-27 15:23:56 +0000

    largest and smallest of array

--- Program_5.c
+++ Program_5.c
@@ -6,12 +6,26 @@
 int main()
 {
     int arr[10], n, i;
+    printf("Enter the size of the array\n");
     scanf("%d", &n);
 
-    for(i = 0; i < n; i++)
+    printf("enter the elements of the array\n");
+    for(i = 0; i < n; i++){
         scanf("%d", &arr[i]);
-
+    }
     //Write your code here
-  
+    for(i = 0; i < n; i++){
+      if(arr[0]>arr[i]){
+        arr[0]=arr[i];
+     }
+    }
+    printf("the smallest number is %d\n",arr[0]);
+     
+     for(i = 0; i < n; i++){
+      if(arr[0]<arr[i]){
+        arr[0]=arr[i];
+     }
+     }
+     printf("the largest number is %d\n",arr[0]);
     return 0;
 }
