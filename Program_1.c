36959f94443a603c3e7c97430030904e1f15eb7d
nishanthss <>
2021-05-27 11:48:11 +0000

    array done

--- Program_1.c
+++ Program_1.c
@@ -12,9 +12,19 @@
 int main()
 {
    //Declare all the variables[N, array a, i] used in the code
+   int n,i,ar[10];
    //Read the Value of N using scanf statement
+   printf("Enter the size of the array ");
+   scanf("%d",&n);
    //Read the array elements using for loop and scanf statment
+   printf("Enter the %d elements of the array\n",n);
+   for (i=0;i<n;i++){
+     scanf("%d",&ar[i]);
+   }
    //Print the array elements in using printf statement
-   
+   printf("The elements of the array are\n");
+   for (i=0;i<n;i++){
+     printf("%d  ",ar[i]);
+   }
    return 0; 
 }
