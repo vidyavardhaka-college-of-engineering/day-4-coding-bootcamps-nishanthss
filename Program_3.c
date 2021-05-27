6d71ef419991c1a721ef43588afabe81efef507e
nishanthss <>
2021-05-27 12:30:53 +0000

    2D array

--- Program_3.c
+++ Program_3.c
@@ -10,8 +10,27 @@
 int main()
 {
   //Declare all the variables[row, col, array a(2D array), i] used in the code
+  int arr[10][10],n,m,j,i;
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
+   printf("Enter the size of row and column of matrix \n");
+   scanf("%d%d",&n,&m);
    //Read the elements of matrix a using nested for loop and scanf statment
+   printf("Enter the elements of matrix \n");
+   for(i=0;i<n;i++){
+      for(j=0;j<m;j++){
+        scanf("%d",&arr[i][j]);
+      }
+      printf("\n");
+
+   }
    //Print the elements of matrix a using nested for loop and printf statment as per the above specified format
+   for(i=0;i<n;i++){
+      for(j=0;j<m;j++){
+        printf("%d ",arr[i][j]);
+      }
+      printf("\n");
+
+   }
+
   return 0;
 }
